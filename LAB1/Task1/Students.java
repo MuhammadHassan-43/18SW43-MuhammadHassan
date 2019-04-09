13bda991e57312f4e9fe46219600de9c3aeeab60
MuhammadHassan-43 <49345368+MuhammadHassan-43@users.noreply.github.com>
2019-04-09 08:09:42 +0500

    Add files via upload

---
+++ LAB1/Task1/Students.java
@@ -0,0 +1,25 @@
+class Students
+{
+public static void main(String args[])
+{
+String name="M.Hassan";
+short rno=43;
+char grade='A';
+double per=98.8;
+int marks=630;
+boolean status;
+System.out.println("Name="+name);
+System.out.println("Roll No:"+rno);
+System.out.println("Percentage="+per+"%");
+System.out.println("Grade="+grade);
+if(status=true)
+{
+System.out.println("Status=Pass");
+}
+else if(status=false) 
+{
+System.out.println("Status=Fail");
+}
+System.out.println("Obtain Marks="+marks);
+}
+}
---
+++ LAB1/Task2/Variable.java
@@ -0,0 +1,13 @@
+class Variable
+{
+public static void main(String[]args)
+{
+double value=50.25;
+long part=(long)value;
+double fpart=value-part;
+
+System.out.println("Actual Number="+value);
+System.out.println("Integer Part="+part);
+System.out.println("Fractinal Part="+fpart);
+}
+}
