d0e523ee8726ba7361a1a1e2c2f3bc62b167a28a
MuhammadHassan-43 <49345368+MuhammadHassan-43@users.noreply.github.com>
2019-04-17 08:14:11 +0500

    Update Task1.java

--- LAB2/Task1/Task1.java
+++ LAB2/Task1/Task1.java
@@ -1,6 +1,6 @@
 class Task1{
      public static void main(String args[]){
-         System.out.println("Argument one = "+args[0]);
-         System.out.println("Argument two = "+args[1]);
+         System.out.println("Name = "+args[0]);
+         System.out.println("Roll No = "+args[1]);
     }
 }
